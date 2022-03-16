6fe75e456c005bf7143ee5024e966554bf898dd5
krishnavamsi21 <knvkrishnavamsi@gmail.com>
2022-03-16 16:55:15 +0530

    refactor_project

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,31 @@
 import 'package:flutter/material.dart';
 
+import 'todoscreen.dart';
+import 'todo.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter DropDownButton',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-// Initial Selected Value
-  String dropdownvalue = 'JOIN ROOM';
-
-// List of items in our dropdown menu
-  var items = [
-    'JOIN ROOM',
-    'CREATE ROOM',
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("ROOMS"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            DropdownButton(
-              // Initial Value
-              value: dropdownvalue,
-
-              // Down Arrow Icon
-              icon: const Icon(Icons.keyboard_arrow_down),
-
-              // Array list of items
-              items: items.map((String items) {
-                return DropdownMenuItem(
-                  value: items,
-                  child: Text(items),
-                );
-              }).toList(),
-              // After selecting the desired option,it will
-              // change button value to selected value
-              onChanged: (String? newValue) {
-                setState(() {
-                  dropdownvalue = newValue!;
-                });
-              },
-            ),
-          ],
-        ),
+      title: 'Passing Data',
+      home: TodosScreen(
+        todos: List.generate(
+            2,
+            (i) => (i == 0)
+                ? const Todo(
+                    'JOIN ROOM ',
+                    'A description of what needs to be done for Todo ',
+                  )
+                : const Todo(
+                    'CREATE ROOM',
+                    '....',
+                  )),
       ),
     );
   }
---
+++ lib/views/todoscreen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'details.dart';
+
+import 'todo.dart';
+
+class TodosScreen extends StatelessWidget {
+  const TodosScreen({Key? key, required this.todos}) : super(key: key);
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('krishna vamsi'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            // When a user taps the ListTile, navigate to the DetailScreen.
+            // Notice that you're not only creating a DetailScreen, you're
+            // also passing the current todo through to it.
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const DetailScreen(),
+                  // Pass the arguments as part of the RouteSettings. The
+                  // DetailScreen reads the arguments from these settings.
+                  settings: RouteSettings(
+                    arguments: todos[index],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
